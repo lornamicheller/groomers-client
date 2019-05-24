f82e6d5ee3973025b8a73c518bc42870e58cdabb
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2019-05-24 15:18:51 -0400

    commit

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import Parse from 'parse';
+// import Parse from 'parse';
 import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
 
-let parse = require('parse');
+// let parse = require('parse');
 
 @Component({
   selector: 'app-splash',
@@ -13,8 +13,8 @@ let parse = require('parse');
 export class SplashPage implements OnInit {
 
   constructor(public NavCtrl:NavController, public nativePageTransitions: NativePageTransitions) {
-    parse.serverURL = 'https://parseapi.back4app.com/';
-    Parse.initialize("2lVOPqiL2Ea7bit43NHkjtF0R5UFKxtiUuO8aqnr", "zGjEtvRTwS0a559cBZBvYQjMN58ZxpXNGrKxLfBp");
+    // parse.serverURL = 'https://parseapi.back4app.com/';
+    // Parse.initialize("2lVOPqiL2Ea7bit43NHkjtF0R5UFKxtiUuO8aqnr", "zGjEtvRTwS0a559cBZBvYQjMN58ZxpXNGrKxLfBp");
    }
 
   ngOnInit() {
@@ -23,22 +23,22 @@ export class SplashPage implements OnInit {
   }
 
   goHome() {
-    Parse.User.currentAsync().then(user => {
-      console.log('Logged user', user);
+    // Parse.User.currentAsync().then(user => {
+    //   console.log('Logged user', user);
   
-      user != null ? this.NavCtrl.navigateRoot('/tutorial') : this.NavCtrl.navigateRoot('/tutorial');
-    }, err => {
-      console.log('Error getting logged user',err);
-      let options: NativeTransitionOptions = {
-        direction: 'left',
-        duration: 200,
-        slowdownfactor: -1,
-        slidePixels: 20,
-        iosdelay: 100, 
-        androiddelay: 100,
-       }
-      });
-   // this.NavCtrl.navigateRoot("/tutorial");
+    //   user != null ? this.NavCtrl.navigateRoot('/tutorial') : this.NavCtrl.navigateRoot('/tutorial');
+    // }, err => {
+    //   console.log('Error getting logged user',err);
+    //   let options: NativeTransitionOptions = {
+    //     direction: 'left',
+    //     duration: 200,
+    //     slowdownfactor: -1,
+    //     slidePixels: 20,
+    //     iosdelay: 100, 
+    //     androiddelay: 100,
+    //    }
+    //   });
+   this.NavCtrl.navigateRoot("/tutorial");
   }
 
 }
